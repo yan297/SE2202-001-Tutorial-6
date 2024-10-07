63ddb0e5dca49f16ffe9ea6ec3173e055de9a2c2
Zhichen Yan <145048700+yan297@users.noreply.github.com>
2024-10-07 08:59:16 -0400

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Tutorial 5: DOM objects and Events
+
+In this tutorial you'll practice working with the DOM objects and events while creating a simple calculator
---
+++ script.js
@@ -0,0 +1,33 @@
+// use DOM queries to get access to the DOM elements that you need
+
+// link the displayDigit function to the click event of the digit buttons
+
+
+//link the operationClicked function to the click even of the operation buttons
+
+
+// Define the displayDigit function to be added as an event listener to the buttons representing the
+// digits so that when the button is clicked the digit it represents would be concatenated
+// to the contents of the display label
+function displayDigit
+
+// set the click event of the clear button
+// to an anonymous function that clears the text of the display lable
+
+
+let firstValue, secondValue, operation;
+
+//Define the operationClicked function to handle the click event of the operation buttons
+//The function body should include the following steps:
+    //Convert the value in the display label into a number and store it in the variable firstValue
+    //Store the text of the clicked button in the variable 'operation' for latter reference
+    //Clear the display lable to allow the user to enter the second value
+function operationClicked
+
+
+//Define the equalClicked function to handlet the click event of the equal button with the following steps
+    //Convert the value in the display label to a number and store it in the variable secondValue
+    // based on the value stored in the operation apply the corresponding operator on the first and second values
+    // and display the result in the display label.
+
+function equalClicked()
